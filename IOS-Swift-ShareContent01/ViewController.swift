2d11501a188f9b0c746526c00bcd028ea336c204
Pooya Hatami <pooya@node-1w7jr9qrok3i9p5vcbqtrebd6.ipv6.telus.net>
2018-03-20 17:01:23 -0700

    file sharing doesn't work yet ..

--- IOS-Swift-ShareContent01/ViewController.swift
+++ IOS-Swift-ShareContent01/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIDocumentInteractionControllerDelegate {
 
+    final private let stringWithLink = "Please download this app here in App Store: https://google.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func shareStringTapped(_ sender: Any) {
+        let activityController = UIActivityViewController(activityItems: [stringWithLink], applicationActivities: nil)
+        
+        activityController.completionWithItemsHandler = { (nil, completed, _, error) in
+            if completed {
+                print("completed")
+            } else {
+                print("cancled")
+            }
+        }
+        present(activityController, animated: true) {
+            print("presented")
+        }
+    }
+    
+    
+    @IBAction func shareImageTapped(_ sender: Any) {
+        guard let image = UIImage(named: "shareME") else { return }
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        activityController.completionWithItemsHandler = { (nil, completed, _, error) in
+            if completed {
+                print("completed")
+            } else {
+                print("cancled")
+            }
+        }
+        present(activityController, animated: true) {
+            print("presented")
+        }
+    }
+    
+    
+    @IBAction func sharePDFTapped(_ sender: Any) {
+        guard let url = Bundle.main.url(forResource: "sharePDF", withExtension: "pdf") else { return }
+        print(url)
+        let controller = UIDocumentInteractionController(url: url)
+        controller.delegate = self
+        controller.presentPreview(animated: true)
+    }
+    
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+    
 }
 
